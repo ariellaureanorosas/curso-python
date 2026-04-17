47f2aa3aff16502a0e9de5006a3ce87a3d3cbc03
Ariel Rosas <ariellaureanorosas@gmail.com>
2026-04-16 22:10:48 -0300

    subindo aula 9

---
+++ aula9.py
@@ -0,0 +1,23 @@
+num1 = 100
+num2 = 10
+
+adicao = num1 + num2
+print('adição', adicao)
+
+subtracao = num1 - num2
+print('subtração', subtracao)
+
+multiplicacao = num1 * num2
+print('multiplicação', multiplicacao)
+
+divisao = num1 / num2
+print('divisão', divisao)
+
+divisao_inteira = num1 // num2
+print('Divisão Inteira', divisao_inteira)
+
+exponenciacao = num1 ** num2
+print('Exponenciação:', exponenciacao)
+
+modulo = 55 % 2 # Resto da divisão
+print('Modulo:', modulo)
