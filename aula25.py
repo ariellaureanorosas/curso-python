d8796c0cb8718c5f540b13bdf004788cc32b1edb
Ariel Rosas <ariellaureanorosas@gmail.com>
2026-04-17 05:55:47 -0300

    Subindo aula 25

---
+++ aula25.py
@@ -0,0 +1,25 @@
+"""
+Formatação básica de strings
+
+s - string
+d - int
+f - float
+.<número de dígitos>f
+x ou X - Hexadecimal
+(Caractere)(><^)(quantidade)
+> - Esquerda
+< - Direita
+^ - Centro
+= - Força o número a aparecer antes dos zeros
+Sinal - + ou -
+Ex.: 0>-100,.1f
+Conversão flags - !r !s !a
+"""
+
+variavel = 'ABC'
+print(f'{variavel}')
+print(f'{variavel: >10}')
+print(f'{variavel: <10}')
+print(f'{variavel: ^10}')
+print(f'{variavel: >10}')
+print(f'{10000.123018974908:0=+10,.1f}')
