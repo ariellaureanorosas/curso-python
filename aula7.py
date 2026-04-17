77bdb0c1500fc2bd2d5fcdf7fcd0e42e5d2be051
Ariel Rosas <ariellaureanorosas@gmail.com>
2026-04-16 21:47:32 -0300

    correção aula 7 e subindo aula 8

--- aula7.py
+++ aula7.py
@@ -3,6 +3,12 @@
 # O sinal de = é o operador de atribuição. Ele é usado para atribuir um valor a um nome (variável).
 # Uso: nome_variavel = expressão
 
-nome_completo = 'Ariel Laureano Rosas'
-soma = 2+2
-print(nome_completo, soma)
+# nome_completo = 'Ariel Laureano Rosas'
+# soma = 2+2
+# print(nome_completo, soma)
+
+nome = 'Ariel'
+idade = 18
+maior_de_idade = idade>=18
+print('nome:', nome, 'idade:', idade)
+print('é maior de idade?', maior_de_idade)
---
+++ aula8.py
@@ -0,0 +1,13 @@
+nome = 'Ariel'
+sobrenome = 'Laureano Rosas'
+idade = 19
+ano_nascimento = 2026 - idade
+maior_idade = ano_nascimento > 18
+altura = 1.80
+
+print('nome:', nome)
+print('sobrenome:', sobrenome)
+print('idade:', idade)
+print('ano de nascimento:', ano_nascimento)
+print('maior de idade?', maior_idade)
+print('Altura em metros:', altura)
