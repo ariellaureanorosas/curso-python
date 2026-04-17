ad510e50057fb0c6ef39433b2502a99751cd3470
Ariel Rosas <ariellaureanorosas@gmail.com>
2026-04-16 23:51:38 -0300

    subindo aula 15

---
+++ aula15.py
@@ -0,0 +1,12 @@
+# nome = input('qual o seu nome? ')
+# print(f'seu nome é {nome}')
+# numero1 = int(input('digite um numero '))
+# numero2 = int(input('Digite outro numero '))
+
+numero1 = input('digite um numero ')
+numero2 = input('Digite outro numero ')
+
+int_numero_1 = int(numero1)
+int_numero_2 = int(numero2)
+
+print(f'A soma dos números é: {int_numero_1 + int_numero_2}')
