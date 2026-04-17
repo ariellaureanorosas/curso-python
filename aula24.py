0e517d8ad24bb998e917db16a0dc26bd18c7a7bc
Ariel Rosas <ariellaureanorosas@gmail.com>
2026-04-17 05:44:03 -0300

    Subindo aula 24

---
+++ aula24.py
@@ -0,0 +1,11 @@
+# Interpolação básica de strings
+# s - string
+# d e i - int
+# f - float
+# x e X- Hexadecimal (ABCDEF0123456789)
+
+nome = 'Ariel'
+preco = 1000.12398712
+variavel = '%s, o preço é R$%.2f' % (nome, preco)
+print(variavel)
+print('O hexadecimal de %d é %08X' % (15123, 15123))
