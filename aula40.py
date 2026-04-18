d698c274bbaa574a0b0c47f027d56bc6c7fb2b0e
Ariel Rosas <ariellaureanorosas@gmail.com>
2026-04-18 08:28:20 -0300

    subindo aula 40

---
+++ aula40.py
@@ -0,0 +1,56 @@
+"""Calculadora com while"""
+
+while True:
+    numero_1 = input("Digite um número: ")
+    numero_2 = input("Digite outro número: ")
+    operador = input("Digite o operador (+-/*): ")
+
+    # ------------------CHECAGEM DOS NÚMEROS------------------#
+    numeros_validos = None
+    try:
+        num_1_float = float(numero_1)
+        num_2_float = float(numero_2)
+        numeros_validos = True
+    except:
+        numeros_validos = None
+
+    if numeros_validos is None:
+        print("Um ou ambos os números digitados são inválidos.")
+        continue
+
+    # --------------CHECAGEM DE OPERADORES-------------------#
+    operadores_permitidos = "+-/*"
+
+    if operador not in operadores_permitidos:
+        print("Operador inválido.")
+        continue
+
+    if len(operador) > 1:
+        print("Digite apenas um operador.")
+        continue
+
+    # -------------------OPERAÇÃO---------------------------#
+    if operador == "+":
+        print(
+            f"O resultado entre {numero_1} e {numero_2} é {num_1_float + num_2_float}"
+        )
+    elif operador == "-":
+        print(
+            f"O resultado entre {numero_1} e {numero_2} é {num_1_float - num_2_float}"
+        )
+    elif operador == "*":
+        print(
+            f"O resultado entre {numero_1} e {numero_2} é {num_1_float * num_2_float}"
+        )
+    elif operador == "/":
+        print(
+            f"O resultado entre {numero_1} e {numero_2} é {num_1_float / num_2_float}"
+        )
+
+    # -------------------CHECAGEM DE SAÍDA---------------------------#
+    saida = input("Você quer sair: [S] ou [N] ").strip().lower()
+    checagem_saida = saida == "s" or saida.__contains__("sim")
+
+    if checagem_saida == True:
+        print("Você decidiu Sair!")
+        break
