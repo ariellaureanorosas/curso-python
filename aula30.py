79b20f537de41d5490ab782092c4a1dacf0f6d2c
Ariel Rosas <ariellaureanorosas@gmail.com>
2026-04-17 22:35:34 -0300

    Subindo aula 31 e alterações na aula 30

--- aula30.py
+++ aula30.py
@@ -11,10 +11,15 @@ RADAR_1 = 60  # velocidade máxima do radar 1
 LOCAL_1 = 100  # local onde o radar 1 está
 RADAR_RANGE = 1  # A distância onde o radar pega
 
-if velocidade > RADAR_1:
+velocidade_carro_radar_1 = velocidade > RADAR_1
+carro_passou_radar_1 = local_carro >= (LOCAL_1 - RADAR_RANGE) and local_carro <= (LOCAL_1 + RADAR_RANGE)
+carro_multado = carro_passou_radar_1 and velocidade_carro_radar_1
+
+if velocidade_carro_radar_1:
     print('Velocidade carro passou do radar 1')
 
-if local_carro >= (LOCAL_1 - RADAR_RANGE) and \
-    local_carro <= (LOCAL_1 + RADAR_RANGE) and \
-        velocidade > RADAR_1:
+if carro_passou_radar_1:
+    print('Carro passou do Radar 1')
+
+if carro_passou_radar_1 and velocidade_carro_radar_1:
     print('Carro multado no Radar 1')
