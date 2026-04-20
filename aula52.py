8da3d3bb1db2c71da9d06a45ab4558ac705df434
Ariel Rosas <ariellaureanorosas@gmail.com>
2026-04-20 08:36:44 -0300

    subindo aula 52

---
+++ aula52.py
@@ -0,0 +1,13 @@
+"""
+Exercício
+Exiba os índices da lista
+0 Maria
+1 Helena
+2 Luiz
+"""
+
+lista_nomes = ["Ariel", "Laureano", "Rosas"]
+indices = range(len(lista_nomes))
+
+for indice in indices:
+    print(indice, lista_nomes[indice])
