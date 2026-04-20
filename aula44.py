e221a7b1cae49df978386e6ea11e584285599bc8
Ariel Rosas <ariellaureanorosas@gmail.com>
2026-04-20 04:22:49 -0300

    Subindo aula 44

---
+++ aula44.py
@@ -0,0 +1,10 @@
+"""
+For + Range
+range -> range(start, stop, step)
+"""
+
+# quantidade_de_numeros = int(input("Digite a quantide de numeros no range: "))
+numeros = range(0, 10, 1)
+
+for numero in numeros:
+    print(numero)
