c21412a34efb7372608614de76596eb37cd2561f
Ariel Rosas <ariellaureanorosas@gmail.com>
2026-04-20 07:42:05 -0300

    Subindo alterações da aula 48

--- aula48.py
+++ aula48.py
@@ -5,3 +5,7 @@ Suporta vários valores de qualquer tipo
 Conhecimentos reutilizáveis - índices e fatiamento
 Métodos úteis: append, insert, pop, del, clear, extend, +
 """
+
+string = "Ariel"
+lista = [123, "Ariel Rosas", True, 1.2, []]
+# print(bool(lista)) -> falsy
