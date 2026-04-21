0a66f379403a57e3bba13e05dbf7fdee5ab9642a
Ariel Rosas <ariellaureanorosas@gmail.com>
2026-04-21 19:44:36 -0300

    subindo alterações da aula 56

--- aula56.py
+++ aula56.py
@@ -16,27 +16,33 @@ while True:
     opcao = input("Selecione uma opção:\n[I]nserir [A]pagar [L]istar [S]air: ").lower()
     print("-" * 30)
 
+    def limpar():
+        os.system("cls" if os.name == "nt" else "clear")
+
     if opcao not in ("i", "a", "l", "s"):
         print("Digite a entrada corretamente")
+        continue
 
     if opcao == "i":
         adiciona_item = input("Adicione o item: ".capitalize())
-        os.system("cls" if os.name == "nt" else "clear")
+        limpar()
         lista_compras.append(adiciona_item.strip())
 
     elif opcao == "a":
-        if len(lista_compras) == 0:
+        if not lista_compras:
             print(
                 "Não tem como apagar, pois ainda não existe valores. Por favor insira um valor"
             )
+            continue
         else:
             try:
-                os.system("cls" if os.name == "nt" else "clear")
+                limpar()
                 for indice, valor in enumerate(lista_compras):
                     print(f"[{indice}] - {valor}")
                 print("-" * 30)
                 apagar_item_lista = int(input("Digite o indice que quer apagar: "))
-                lista_compras.pop(apagar_item_lista)
+                item_removido = lista_compras.pop(apagar_item_lista)
+                print(f"Removido - {item_removido}")
             except ValueError:
                 print("Digite apenas números")
             except IndexError:
@@ -45,8 +51,8 @@ while True:
                 print("Erro desconhecido")
 
     elif opcao == "l":
-        os.system("cls" if os.name == "nt" else "clear")
-        if len(lista_compras) == 0:
+        limpar()
+        if not lista_compras:
             print("Não tem nenhum valor a ser exibido, por favor adicione a lista")
         else:
             for indice, valor in enumerate(lista_compras):
