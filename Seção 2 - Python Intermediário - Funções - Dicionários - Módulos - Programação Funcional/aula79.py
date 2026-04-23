e09abb58419d8b3a1291a722ccf047b8a805d4b5
Ariel Rosas <ariellaureanorosas@gmail.com>
2026-04-23 13:43:23 -0300

    Subindo exercício feito por mim da aula 79

--- Seção 2 - Python Intermediário - Funções - Dicionários - Módulos - Programação Funcional/aula79.py
+++ Seção 2 - Python Intermediário - Funções - Dicionários - Módulos - Programação Funcional/aula79.py
@@ -18,37 +18,72 @@ perguntas = [
         "Resposta": "5",
     },
 ]
+# qtd_acertos = 0
+# for pergunta in perguntas:
+#     print(pergunta["Pergunta"])
+#     print()  # pula uma linha
+
+#     opcoes = pergunta["Opções"]
+#     for indice, opcao in enumerate(opcoes):
+#         print(f"{indice})", opcao)
+#     print()  # pula uma linha
+
+#     escolha = input("Digite uma opção: ")
+#     escolha_int = None
+#     acertou = False
+#     qtd_opcoes = len(opcoes)
+
+#     if escolha.isdigit():
+#         escolha_int = int(escolha)
+
+#     if escolha_int is not None:
+#         if escolha_int >= 0 and escolha_int < qtd_opcoes:
+#             if opcoes[escolha_int] == pergunta["Resposta"]:
+#                 acertou = True
+
+#     if acertou:
+#         print()  # pula uma linha
+#         print("Acertou")
+#         print()  # pula uma linha
+#         qtd_acertos += 1
+#     else:
+#         print()  # pula uma linha
+#         print("Errou")
+#         print()  # pula uma linha
+
+# print("QUANTIDADE DE ACERTOS:", qtd_acertos, "DE:", len(perguntas))
+import os
+
 qtd_acertos = 0
+
 for pergunta in perguntas:
-    print(pergunta["Pergunta"])
-    print()  # pula uma linha
-
-    opcoes = pergunta["Opções"]
-    for indice, opcao in enumerate(opcoes):
-        print(f"{indice})", opcao)
-    print()  # pula uma linha
-
-    escolha = input("Digite uma opção: ")
-    escolha_int = None
-    acertou = False
-    qtd_opcoes = len(opcoes)
-
-    if escolha.isdigit():
-        escolha_int = int(escolha)
-
-    if escolha_int is not None:
-        if escolha_int >= 0 and escolha_int < qtd_opcoes:
-            if opcoes[escolha_int] == pergunta["Resposta"]:
-                acertou = True
-
-    if acertou:
+    while True:
+        print(pergunta["Pergunta"])
         print()  # pula uma linha
-        print("Acertou")
+        for i, opcao in enumerate(pergunta["Opções"]):
+            print(f"{i})", opcao)
         print()  # pula uma linha
-        qtd_acertos += 1
-    else:
-        print()  # pula uma linha
-        print("Errou")
-        print()  # pula uma linha
+        entrada = input("Digite a alternativa correta: ")
+        try:
+            entrada_int = int(entrada)
+            resposta = entrada_int
+            opcoes = pergunta["Opções"]
+            if resposta >= 0 and resposta < len(pergunta["Opções"]):
+                if (
+                    opcoes[resposta] == pergunta["Resposta"]
+                ):  # -> mesma coisa de dizer: pergunta["Opções"][escolha_int]
+                    print("Acertou")
+                    print()  # pula uma linha
+                    qtd_acertos += 1
+                else:
+                    print("Errou")
+                    print()  # pula uma linha
+                break
+            else:
+                print("Alternativa Inválida, por Favor digite novamente")
+        except:
+            os.system("cls" if os.name == "nt" else "clear")
+            print("Entrada de dados inválida, digite corretamente por favor")
+            print()  # pula uma linha
 
-print("QUANTIDADE DE ACERTOS:", qtd_acertos, "DE:", len(perguntas))
+print("Quantidade de acertos", qtd_acertos)
