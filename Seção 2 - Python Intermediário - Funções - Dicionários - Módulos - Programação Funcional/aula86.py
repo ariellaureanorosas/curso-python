ed7b6c712b197595b22df5afad3da58ceddd8178
Ariel Rosas <ariellaureanorosas@gmail.com>
2026-04-24 05:08:17 -0300

    Subindo aula 86

--- Seção 2 - Python Intermediário - Funções - Dicionários - Módulos - Programação Funcional/aula86.py
+++ Seção 2 - Python Intermediário - Funções - Dicionários - Módulos - Programação Funcional/aula86.py
@@ -11,6 +11,55 @@
 
 
 # lista = [numero for numero in range(10)]
-lista = [numero * 2 for numero in range(10)]
+# lista = [numero * 2 for numero in range(10)]
 
-print(lista)
+# print(lista)
+
+import pprint
+
+
+def p(v):
+    pprint.pprint(v, sort_dicts=False, width=40)
+
+
+# Mapeamento de dados em list comprehension
+produtos = [
+    {
+        "nome": "p1",
+        "preco": 20,
+    },
+    {
+        "nome": "p2",
+        "preco": 10,
+    },
+    {
+        "nome": "p3",
+        "preco": 30,
+    },
+]
+# MAPEAMENTO
+novos_produtos = [
+    (
+        {**produto, "preco": produto["preco"] * 2}
+        if produto["preco"] > 20
+        else {**produto}
+    )
+    for produto in produtos
+]
+
+# print(*novos_produtos, sep="\n")
+# lista = [n for n in range(10) if n < 5]
+
+# FILTRO
+novos_produtos = [{**produto} for produto in produtos if produto["preco"] > 10]
+# Posso combinar o filter com o mapeamento
+novos_produtos = [
+    (
+        {**produto, "preco": produto["preco"] * 1.05}
+        if produto["preco"] >= 20
+        else {**produto}
+    )
+    for produto in produtos
+    if (produto["preco"] >= 20 and produto["preco"] * 1.05) > 10
+]
+p(novos_produtos)
