2ad6aa00b14e6be4816d5fb292a507e230b3fcdd
Ariel Rosas <ariellaureanorosas@gmail.com>
2026-05-01 23:36:47 -0300

    Subindo aula 101 e seus modulos

---
+++ Seção 2 - Python Intermediário - Funções - Dicionários - Módulos - Programação Funcional/aula101_main.py
@@ -0,0 +1,12 @@
+from sys import path
+
+import aula101_package.modulo
+
+# from aula101_package.modulo import soma_do_modulo
+from aula101_package import modulo
+from aula101_package.modulo import *
+
+print(soma_do_modulo(1, 2))
+print(aula101_package.modulo.soma_do_modulo(1, 2))
+print(modulo.soma_do_modulo(1, 2))
+print(variavel)
---
+++ Seção 2 - Python Intermediário - Funções - Dicionários - Módulos - Programação Funcional/aula101_package/modulo.py
@@ -0,0 +1,7 @@
+__all__ = ["variavel", "soma_do_modulo"]
+
+variavel = "Ariel"
+
+
+def soma_do_modulo(a, b):
+    return a + b
